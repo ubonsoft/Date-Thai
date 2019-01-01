ea2f4a711ddc117f990d55822cf7a5d0b2e36f4c
อานนท์ พิมายนอก <pimainok3@gmail.com>
2019-01-01 21:57:01 +0700

    DateTH
    TH

---
+++ Date.js
@@ -0,0 +1,9 @@
+function getDateTime(){
+    var now = new Date().toLocaleTimeString('en-US', {
+      timeZone: 'Asia/Bangkok',
+      hour12 : false,
+      formatMatcher : "basic"
+    });
+    let currentDate = now;
+    return currentDate;
+}
